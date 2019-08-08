e9b52d8819ee5aa7afc42088ddb8988b72aa6386
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-08-08 21:21:54 +0530

    Update q3.java

--- project/q3.java
+++ project/q3.java
@@ -1,20 +1,16 @@
 package project;
-
 public class q3 {
 
 	public static void main(String args[]) {
-		long num=13195;
-		for(long i=2;i<=num/i;i++){
-			while(num%i==0){
-				
-				num=num/i;
-				System.out .println(num);
-				
+		long num=600851475143l;
+		for(long i=3;i<=num/2;i+=2){
+			while(num%i==0)
+			{
+					num=num/i;
 			}
-			System.out.println(i+"");
 		}
 		if(num>1){
-			System.out.println("largest primr factor is"+"  "+num);
+			System.out.println("largest prime factor is"+"  "+num);
 			
 		}
 		
