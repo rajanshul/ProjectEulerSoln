3f016f697b15dbd146d7e2a4361e293c1e44fe1a
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-08-11 12:11:41 +0530

    Update q10.java

--- project/q10.java
+++ project/q10.java
@@ -1,30 +1,31 @@
 package project;
 
-import java.util.Arrays;
+public class demo10 {
 
-public class q10 {
-	static  int sumof(int n){
-		boolean prime []=new boolean[n+1];
-	 	Arrays.fill(prime, true);
-	for(int p=2;p*p<=n;p++){
-		if(prime[p]== true){
-		for (int i=p*2;i<=n;i=i+p)
-				prime[i]=false;
-		}
-	}
-	int sum=0;
-	for(int i=2;i<=n;i++)
-		if(prime[i])
-			sum=sum+i;
-	return sum;
-	}
-
-
-	public static void main(String[] args) {
-		int n=2000000;
-		System.out.println(sumof(n));
-		
+		public static void main(String[] args) {
+		    long n=3,s=2;
+		    while(n!=2000000){
+		    	if(n%2!=0 && isprime(n)){
+		    		s+=n;
+		    	     n++; 
+		    }else
+		    	n++;
+		    	}
+		    System.out .println(" the sum"+s);
+		  }
+public static boolean isprime(long n){
+	int c=0;
+	for(int i=1;i<=(int)Math.sqrt(n);i+=2)
+		if(n%i==0)
+			c++;
+	if(c>1)
+		return false;
+	else 
+		return true;
 }
 	}
-	
+
+
+
+
 
