f597e1e008760d4f21acdbbc5b63ffdb39754fd7
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-08-12 13:47:35 +0530

    Update q5.java

--- project/q5.java
+++ project/q5.java
@@ -2,26 +2,20 @@ package project;
 
 public class q5 {
 public static  int p5(){
-
-	int i=2520;
-	boolean found=false;
-	while (!found){
-		i=i+20;
-		boolean isd=true;
-		for(int j=11;j<=20;j++){
-			if(i%j!=0){
-				isd=false;
-				break;
-			}
-		}
-		if(isd){
-			found=true;
-		}
+	int i,j;
+   for( i=2521;;i++){
+for( j=1;j<=20;j++){
+if(i%j==0)
+continue;
+else 
+break;
+}
+if(j==21)
+break;}
+		 
+return i;
 	}
-	return i;
-	}
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		System.out .println(p5());
 
 	}
