7d21cc09d429d056251bf56c8a470ce770dc0f66
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-08-14 23:10:06 +0530

    Update q5.java

--- project/q5.java
+++ project/q5.java
@@ -1,23 +1,25 @@
 package project;
 
 public class q5 {
-public static  int p5(){
-	int i,j;
-   for( i=2521;;i++){
-for( j=1;j<=20;j++){
-if(i%j==0)
-continue;
-else 
-break;
-}
-if(j==21)
-break;}
-		 
-return i;
-	}
-public static void main(String[] args) {
-		System.out .println(p5());
+static long find(long x,long y)
+	{
+		if(x%y!=0)
+			return find(y,x%y);
+				else
+					return y;
+			}
+		static long lcm(long n){
+			long result=1;
+			for(int i=1;i<=n;i++){
+				result=(result*i)/(find(result,i));
+			}
+			return result;
+		}
+	
 
+	public static void main(String[] args) {
+		long n=20;
+		System.out.println(lcm(n));
 	}
 
 }
