9d68d23a37ed4913c1f9dd262cb391ea3529b310
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-08-23 00:51:48 +0530

    Update q_7.java

--- project/q_7.java
+++ project/q_7.java
@@ -5,7 +5,7 @@ public class q_7 {
 	public static void main(String[] args) {
 		int primeno=0;
 		int count=0;
-		for( int num=2;num<10000000;num++){
+		for( int num=2;;num++){
 			boolean pn=true;		
 			for (int j=2;j<num;j++){
 			if(num%j==0){
