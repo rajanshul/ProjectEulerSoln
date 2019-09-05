957387014774f987c7e44132e988118734921cfe
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-09-06 01:30:56 +0530

    Update q10.java

--- project/q10.java
+++ project/q10.java
@@ -2,29 +2,25 @@ package project;
 
 public class q10 {
 public static void main(String[] args){
-		long sum =2;
+		Integer sum=new Integer(2);
+		boolean isprime=true;
 		for(int i=3;i<2000000;i++){
-			if(prime(i))
-			{
-				sum=sum+i;
-			}
-		}System.out.println(sum);
-	
-}
-	static boolean prime(int n)
-	{
-		boolean a=true;
-		for(long i=3;i<=Math.sqrt(n);i=i+2)
-		{
-			if(n%i==0)
-			{
-				a=false;
+		for(int j=2;j<Math.sqrt((double)i);j++){
+			if(i%j==0){
+				isprime=false;
 				break;
-		        }
+			}
 		}
-		return a;
-	
-}
+		if(isprime){
+			sum=sum+Math.add(Integer.valueOf(i));
+		}
+		isprime=true;
+		
+	        }
+		System.out.println(sum);
+
+
+}}
 
 
 
