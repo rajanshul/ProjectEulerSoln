bffa58f362cffcc749ac8d233ad11394e3a949c0
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-09-09 23:35:30 +0530

    Update q10.java

--- project/q10.java
+++ project/q10.java
@@ -4,8 +4,8 @@ public class q10 {
 public static void main(String[] args){
 		Integer sum=new Integer(2);
 		boolean isprime=true;
-		for(int i=3;i<2000000;i++){
-		for(int j=2;j<Math.sqrt((double)i);j++){
+		for(int i=3;i<2000000;i+=2){
+		for(int j=1;j<Math.sqrt((double)i);j+=2){
 			if(i%j==0){
 				isprime=false;
 				break;
