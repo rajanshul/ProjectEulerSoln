5e03cc99173565ba6c9dd9c4176c549171ec26a6
rajanshul <53619520+rajanshul@users.noreply.github.com>
2019-10-30 22:20:20 +0530

    Update q10.java

--- project/q10.java
+++ project/q10.java
@@ -1,26 +1,34 @@
 package project;
 
 public class q10 {
-public static void main(String[] args){
-		Integer sum=new Integer(2);
-		boolean isprime=true;
-		for(int i=3;i<2000000;i+=2){
-		for(int j=3;j<Math.sqrt((double)i);j+=2){
-			if(i%j==0){
-				isprime=false;
-				break;
-			}
-		}
-		if(isprime){
-			sum=sum+Math.add(Integer.valueOf(i));
-		}
-		isprime=true;
-		
-	        }
-		System.out.println(sum);
-
-
-}}
+public static void main(String[] args) {
+      boolean arr[] = new boolean[1000000+1];
+    for(int i=2;i<=1000000;i++){
+        arr[i]=true;
+    }
+        for(int j=2;j*j<=1000000;j++){
+            if(arr[j]){
+                for(int k=j*2;k<=1000000;k+=j){
+                    arr[k]=false;
+                }
+            }
+        }
+        long arr2[] = new long[1000000+1];
+        long sum2=0;
+        for(int f=2;f<=1000000;f++){
+           if(arr[f]){
+               sum2+=f;
+           }
+            arr2[f]=sum2;
+        }
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+           System.out.println(arr2[n]);
+        }
+    }
+}
 
 
 
